13227b36be0353cfd151c6b2dc83b2b7d670f994
Ivan Liang <r33t@cmu.edu>
2025-10-29 17:51:51 -0400

    Add two mock map responses

--- tileset-creator/src/mockData.ts
+++ tileset-creator/src/mockData.ts
@@ -57,8 +57,7 @@ export const mockApiData: Record<string, any> = {
             credentialsCookiesRequired: true,
           },
         ],
-        published: true,
-        footprint: [1, 2, 3],
+        published: false,
       },
       {
         id: 9,
@@ -76,5 +75,46 @@ export const mockApiData: Record<string, any> = {
       },
     ],
     count: 5,
+  },
+  [CONFIG.API_LIST_MAPS + "/maps/public/GHCFloor5/tileset"]: {
+    "name": "GHCFloor5",
+    "description": "",
+    "namespace": "public",
+    "services": [
+      {
+        "url": "/tileset",
+        "name": "tileserver",
+        "credentialsCookiesRequired": true
+      }
+    ],
+    "published": true,
+    "footprint": {
+      "type": "Polygon",
+      "coordinates": [
+        [
+          [-79.9450889307174, 40.4432088727398],
+          [-79.9447395808796, 40.4440293645366],
+          [-79.9440864165033, 40.4440115376873],
+          [-79.9441383746005, 40.4435889906852],
+          [-79.9445473826967, 40.4430215103047],
+          [-79.9450889307174, 40.4432088727398]
+        ]
+      ]
+    },
+    "level": null
+  },
+  [CONFIG.API_LIST_MAPS + "/maps/public/CICFloorOne/tileset"]:{
+    "name": "CICFloorOne",
+    "description": "CIC L floor",
+    "namespace": "public",
+    "services": [
+      {
+        "url": "/tileset",
+        "name": "tileserver",
+        "credentialsCookiesRequired": true
+      }
+    ],
+    "published": true,
+    "footprint": null,
   },
 };
